46a5281e0781b029065f7e02a0b3bf57db18f313
Heyhey <Heyhey@NAME>
2019-07-06 14:47:44 +0500

    Добавьте файлы проекта.

---
+++ BarsHM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarsHM.Startup))]
+namespace BarsHM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
